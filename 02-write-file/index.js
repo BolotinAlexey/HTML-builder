539b0829ae2677db3924414c76a8e534631dcb41
Oleksii Bolotin <samkiral73@gmail.com>
2024-01-18 20:37:43 +0100

    refactor: modify 02-write-file using event 'exit'

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,20 +1,18 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { stdin: consoleIn, stdout: consoleOut } = require('node:process');
+const process = require('node:process');
+const { stdin: consoleIn, stdout: consoleOut, exit } = process;
 
 const writeStream = fs.createWriteStream(path.join(__dirname, 'logFile.txt'));
 consoleOut.write('Please type the text in the console...\n');
-
+process.on('exit', () =>
+  consoleOut.write('\nThe process is complete, for now!\n'),
+);
 consoleIn.on('data', (chank) => {
   if (chank.toString() === 'exit\n') {
-    exitFun();
+    exit();
   }
 });
-process.on('SIGINT', exitFun);
-
-function exitFun() {
-  consoleOut.write('\nThe process is complete, for now!\n');
-  process.exit();
-}
+process.on('SIGINT', exit);
 
 consoleIn.pipe(writeStream);
