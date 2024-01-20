d6bdd5476c675b2d39735c9d95c2cb24c098b82d
Oleksii Bolotin <samkiral73@gmail.com>
2024-01-20 22:47:40 +0100

    feat: htmlBuilder is done

---
+++ 06-build-page/index.js
@@ -0,0 +1,68 @@
+const fs = require('node:fs');
+const fss = fs.promises;
+const path = require('node:path');
+const { Transform, pipeline } = require('node:stream');
+
+const htmlSrc = path.join(__dirname, 'components');
+const htmlTemplate = path.join(__dirname, 'template.html');
+const htmlDest = path.join(__dirname, 'project-dist', 'index.html');
+
+(async function htmlBuilder(ext) {
+  let templateStr = '';
+  let specSymbol = '';
+
+  await fss.mkdir(path.join(__dirname, 'project-dist'), { recursive: true });
+  const htmlReadStream = fs.createReadStream(htmlTemplate, {
+    highWaterMark: 1,
+    encoding: 'utf-8',
+  });
+  const htmlWriteStream = fs.createWriteStream(htmlDest);
+
+  const htmlFiles = (await fss.readdir(htmlSrc, { withFileTypes: true }))
+    .filter(
+      (file) =>
+        path.extname(path.join(htmlSrc, file.name)) === ext && file.isFile(),
+    )
+    .map((file) => file.name);
+
+  const ts = new Transform({
+    async transform(char, encoding, cb) {
+      if (specSymbol === '' && char.toString() === '{') {
+        specSymbol = '{';
+        this.push('');
+      } else if (specSymbol === '{' && char.toString() !== '{') {
+        this.push(`{${char.toString()}`);
+      } else if (specSymbol === '{' && char.toString() === '{') {
+        specSymbol = '{{';
+        this.push('');
+      } else if (specSymbol === '{{' && char.toString() !== '}') {
+        templateStr += char.toString();
+        this.push('');
+      } else if (specSymbol === '{{' && char.toString() === '}') {
+        specSymbol = '}';
+        this.push('');
+      } else if (specSymbol === '}' && char.toString() === '}') {
+        const chunk = await insertHtmlComponent(templateStr, htmlFiles, ext);
+        this.push(chunk);
+        specSymbol = '';
+        templateStr = '';
+      } else if (specSymbol === '}' && char.toString() !== '}') {
+        await fss.unlink(htmlDest);
+        throw new Error('Syntax error! Missed symbol "}" for template');
+      } else {
+        this.push(char);
+      }
+
+      cb();
+    },
+  });
+
+  pipeline(htmlReadStream, ts, htmlWriteStream, (err) => {
+    err && console.error(err);
+  });
+})('.html');
+
+async function insertHtmlComponent(str, htmlFiles, ext) {
+  const htmlFile = htmlFiles.find((file) => file.replace(ext, '') === str);
+  return await fss.readFile(path.join(htmlSrc, htmlFile), 'utf-8');
+}
